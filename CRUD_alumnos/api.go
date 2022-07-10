03efa66d1fbfb981e2c363f8925e3b2cf5d8ae76
Jose <jose.porres@utec.edu.pe>
2022-07-10 14:26:41 -0500

    Crud_alumnos done using postman

--- CRUD_alumnos/api.go
+++ CRUD_alumnos/api.go
@@ -12,8 +12,7 @@ import (
 )
 
 type Student struct {
-	Id   string    `json:"id"`
-	Nombre string `json:"name"`
+	Nombre string `json:"nombre"`
 	Dni string `json:"dni"`
 	Direccion string `json:"direccion"`
 	Fecha_nacimiento string `json:"fecha_nacimiento"`
@@ -41,7 +40,10 @@ func connectionDB() *sql.DB{
 
 
 func createAlumno(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+
+	var s Student
+	err := json.NewDecoder(r.Body).Decode(&s)
+	chkError(err)
 
 	db :=connectionDB()
 	defer db.Close()
@@ -50,13 +52,12 @@ func createAlumno(w http.ResponseWriter, r *http.Request) {
 	stmt, err := db.Prepare("INSERT INTO students(nombre, dni, direccion, fecha_nacimiento) VALUES(?,?,?,?)")
 	chkError(err)
 	
-	result,err := stmt.Exec(vars["nombre"], vars["dni"], vars["direccion"], vars["fecha_nacimiento"])
+	result,err := stmt.Exec(s.Nombre, s.Dni, s.Direccion, s.Fecha_nacimiento)
 	chkError(err)
 
-	id,err := result.LastInsertId()
+	id, err := result.LastInsertId()
 	chkError(err)
-	
-	fmt.Println("Last inserted ID is:", id)
+	fmt.Fprintf(w, "Student created with id: %d\n", id)
 }
 
 func getStudents() []*Student {
@@ -69,9 +70,10 @@ func getStudents() []*Student {
 	chkError(err)
 
 	var students []*Student
+
 	for rows.Next() {
 		var student Student
-		err = rows.Scan(&student.Id, &student.Nombre, &student.Dni, &student.Direccion, &student.Fecha_nacimiento)
+		err = rows.Scan(&student.Nombre, &student.Dni, &student.Direccion, &student.Fecha_nacimiento)
 		chkError(err)
 		students = append(students, &student)
 	}
@@ -81,48 +83,56 @@ func getStudents() []*Student {
 
 
 func getSingleStudent(w http.ResponseWriter, r *http.Request) *Student{
-    vars := mux.Vars(r)
+    var a Student
+	err := json.NewDecoder(r.Body).Decode(&a)
+	chkError(err)
 
     db :=connectionDB()
 	defer db.Close()
 	PingDb(db)
 
-	query, err := db.Query("SELECT * FROM students WHERE id = ?", vars["id"])
+	query, err := db.Query("SELECT * FROM students WHERE dni = ?", a.Dni)
+
 	chkError(err)
 
 	var s Student
+
 	for query.Next() {
-		err = query.Scan(&s.Id, &s.Nombre, &s.Dni, &s.Direccion, &s.Fecha_nacimiento)
+		err = query.Scan(&s.Nombre, &s.Dni, &s.Direccion, &s.Fecha_nacimiento)
 		chkError(err)
 	}
-
 	return &s
 }
 
-func updateStudentPage(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	
+func updateStudentPage(w http.ResponseWriter, r *http.Request) int64 {
+	var s Student
+	err := json.NewDecoder(r.Body).Decode(&s)
+	chkError(err)
+
 	db :=connectionDB()
 	defer db.Close()
 	PingDb(db)
 
 	//prepare
-	stmt, err:= db.Prepare("UPDATE students SET nombre = ?, dni = ?, direccion = ?, fecha_nacimiento = ? WHERE id = ?")
+	stmt, err:= db.Prepare("UPDATE students SET nombre = ?, dni = ?, direccion = ?, fecha_nacimiento = ? WHERE dni = ?")
 	chkError(err)
 
 	//execute
-	result,err := stmt.Exec(vars["nombre"], vars["dni"], vars["direccion"], vars["fecha_nacimiento"], vars["id"])
+	result,err := stmt.Exec(s.Nombre, s.Dni, s.Direccion, s.Fecha_nacimiento, s.Dni)
 	chkError(err)
 
 	ro,err := result.RowsAffected()
 	chkError(err)
 
-	fmt.Println(ro)
+	return ro
+
 }
 
 
-func deleteStudent(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
+func deleteStudent(w http.ResponseWriter, r *http.Request) int64{
+	var a Student
+	err := json.NewDecoder(r.Body).Decode(&a)
+	chkError(err)
 	
 	db :=connectionDB()
 	defer db.Close()
@@ -130,17 +140,17 @@ func deleteStudent(w http.ResponseWriter, r *http.Request) {
 
 	//prepare
 
-	stmt, err:= db.Prepare("DELETE FROM students WHERE id = ?")
+	stmt, err:= db.Prepare("DELETE FROM students WHERE dni = ?")
 	chkError(err)
 
 	//execute
-	result,err := stmt.Exec(vars["id"])
+	result,err := stmt.Exec(a.Dni)
 	chkError(err)
 
 	ro,err := result.RowsAffected()
 	chkError(err)
 
-	fmt.Println(ro)
+	return ro
 }
 
 
@@ -150,8 +160,10 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 //CREATE
 func createPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Create Page!")
+	fmt.Fprintf(w, "Create Page!\n")
 	createAlumno(w,r)
+	fmt.Fprintf(w, "Student created")
+
 }
 
 //READ
@@ -169,25 +181,39 @@ func studentPage(w http.ResponseWriter, r *http.Request) {
 func singleStudentPage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Single Student Page: \n")
 	student := getSingleStudent(w, r)
-	if student.Id != ""{
-		json.NewEncoder(w).Encode(student)
+	if student.Nombre != ""{
+		json.NewEncoder(w).Encode(*student)
 	}
-	if student.Id == ""{
+	if student.Nombre == ""{
 		fmt.Fprintf(w, "No student found")
 	}
 }
 
 //UPDATE
 func updatePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Update Page!")
-	updateStudentPage(w, r)
+	fmt.Fprintf(w, "Update Page!\n")
+
+	rowsAffected := updateStudentPage(w, r)
+	if rowsAffected > 0{
+		fmt.Fprintf(w, "Student updated")
+	}
+	if rowsAffected == 0{
+		fmt.Fprintf(w, "Student not updated")
+	}
 }
 
 
 //DELETE
 func deleteStudentPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Delete Page!")
-	deleteStudent(w,r);
+	fmt.Fprintf(w, "Delete Page!\n")
+	
+	rowsAffected := deleteStudent(w,r);
+	if rowsAffected > 0{
+		fmt.Fprintf(w, "Student deleted")
+	}
+	if rowsAffected == 0{
+		fmt.Fprintf(w, "Student not deleted")
+	}
 }
 
 
@@ -196,19 +222,18 @@ func main() {
 	r := mux.NewRouter()
 
 	//CREATE STUDENT
-	r.HandleFunc("/student/{nombre}/{dni}/{direccion}/{fecha_nacimiento}", createPage)
+	r.HandleFunc("/student", createPage).Methods("POST")
 	
 	//READ ALL STUDENTS
 	r.HandleFunc("/students", studentPage).Methods("GET")
 	
 	//READ SINGLE STUDENT
-	r.HandleFunc("/student/{id}", singleStudentPage)
+	r.HandleFunc("/student", singleStudentPage).Methods("GET")
 	
 	//UPDATE STUDENT
-	r.HandleFunc("/student/{id}/{nombre}/{dni}/{direccion}/{fecha_nacimiento}", updatePage)
-
+	r.HandleFunc("/student", updatePage).Methods("PUT")
 	//DELETE STUDENT
-	r.HandleFunc("/delete/{id}", deleteStudent)
+	r.HandleFunc("/student", deleteStudentPage).Methods("DELETE")
 	
 	//HOME PAGE
 
--- CRUD_alumnos/database/migration.sql
+++ CRUD_alumnos/database/migration.sql
@@ -1,16 +1,15 @@
 CREATE TABLE `students`
 (
-    id   bigint auto_increment,
     nombre varchar(255) NOT NULL,
     dni varchar(255) NOT NULL,
     direccion varchar(255) NOT NULL,
     fecha_nacimiento varchar(255) NOT NULL,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`dni`)
 );
 
 INSERT INTO `students` (`nombre`, `dni`,`direccion`,`fecha_nacimiento`) VALUES 
        ('Jose', '12345678', 'Calle falsa 123', '2020-01-01'),
        ('Juan', '87654321', 'Calle verdadera 456', '2020-01-01'),
        ('Adrian','1234124', 'Valle', '2020-01-01'),
-       ('John', '1234124', 'Valle', '2020-01-01'),
-       ('Mary', '1234124', 'Valle', '2020-01-01');
+       ('John', '12345154', 'Valle', '2020-01-01'),
+       ('Mary', '12453124', 'Valle', '2020-01-01');
