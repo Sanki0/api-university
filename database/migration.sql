26a510e5cdffd2f39224d05a84b1318df32c4980
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-07-12 21:56:24 -0500

    feat: added table records to db

--- database/migration.sql
+++ database/migration.sql
@@ -15,6 +15,15 @@ CREATE TABLE `courses`
     PRIMARY KEY (`nombre`)
 );
 
+CREATE TABLE `records`
+(
+    student varchar(255) FOREIGN KEY REFERENCES students(`dni`) ,
+    course varchar(255) FOREIGN KEY REFERENCES courses(`nombre`) ,
+    startdate varchar(255) NOT NULL,
+    finishdate varchar(255) NOT NULL,
+    PRIMARY KEY (`student`,`course` )
+);
+
 INSERT INTO `students` (`nombre`, `dni`,`direccion`,`fecha_nacimiento`) VALUES 
        ('Jose', '12345678', 'Calle falsa 123', '2020-01-01'),
        ('Juan', '87654321', 'Calle verdadera 456', '2020-01-01'),
@@ -28,3 +37,24 @@ INSERT INTO `courses` (`nombre`, `descripcion`, `temas`) VALUES
        ('Python', 'Programacion en Python', 'Python, MySQL, HTML, CSS'),
        ('C#', 'Programacion en C#', 'C#, MySQL, HTML, CSS'),
        ('C++', 'Programacion en C++', 'C++, MySQL, HTML, CSS');
+
+INSERT INTO records (`student`, `course`, `startdate`, `finishdate`) VALUES
+        ('12345678', 'PHP', '2020-01-01', '2020-01-01'),
+        ('12345678', 'Java', '2020-01-01', '2020-01-01'),
+        ('12345678', 'Python', '2020-01-01', '2020-01-01'),
+        ('12345678', 'C#', '2020-01-01', '2020-01-01'),
+        ('12345678', 'C++', '2020-01-01', '2020-01-01'),
+        ('87654321', 'PHP', '2020-01-01', '2020-01-01'),
+        ('87654321', 'Java', '2020-01-01', '2020-01-01'),
+        ('87654321', 'Python', '2020-01-01', '2020-01-01'),
+        ('87654321', 'C#', '2020-01-01', '2020-01-01'),
+        ('87654321', 'C++', '2020-01-01', '2020-01-01'),
+        ('1234124', 'PHP', '2020-01-01', '2020-01-01'),
+        ('1234124', 'Java', '2020-01-01', '2020-01-01'),
+        ('1234124', 'Python', '2020-01-01', '2020-01-01'),
+        ('1234124', 'C#', '2020-01-01', '2020-01-01'),
+        ('1234124', 'C++', '2020-01-01', '2020-01-01'),
+        ('12345154', 'PHP', '2020-01-01', '2020-01-01'),
+        ('12345154', 'Java', '2020-01-01', '2020-01-01'),
+        ('12345154', 'Python', '2020-01-01', '2020-01-01'),
+        ('12345154', 'C#', '2020-01-01', '2020-01-01');
