0900b1cd65c6e28724794440b650955a67711245
joseporres <jose.porres@utec.edu.pe>
2022-08-08 11:32:52 -0500

    feat: creates testing done

--- graph/schema.resolvers_test.go
+++ graph/schema.resolvers_test.go
@@ -61,11 +61,98 @@ func TestCreateStudent(t *testing.T) {
 }
 
 func TestCreateCourse(t *testing.T) {
-	t.Skip("not implemented")
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	
+
+	mock.ExpectExec("INSERT INTO courses").
+	WithArgs("Devmente", "Introduccion", "Go, AWS").
+	WillReturnResult(sqlmock.NewResult(1, 1))
+	
+	r := Resolver{DB: db}
+
+	c:= client.New(handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &r})))
+
+	var resp struct {
+		CreateCourse struct {
+			Nombre string
+			Descripcion string
+			Temas string
+		}
+	}
+
+	c.MustPost(`
+	mutation {
+		createCourse(nombre: "Devmente", descripcion: "Introduccion", temas: "Go, AWS"){
+			nombre
+			descripcion
+			temas
+		}
+	}`, &resp)
+	
+
+
+	require.Equal(t, "Devmente", resp.CreateCourse.Nombre)
+	require.Equal(t, "Introduccion", resp.CreateCourse.Descripcion)
+	require.Equal(t, "Go, AWS", resp.CreateCourse.Temas)
+
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
 }
 
 func TestCreateRecord(t *testing.T) {
-	t.Skip("not implemented")
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	
+	mock.ExpectExec("INSERT INTO records").
+				WithArgs("12453124", "PHP", "2022-09-01", "2022-12-01").
+				WillReturnResult(sqlmock.NewResult(1, 1))
+	
+	r := Resolver{DB: db}
+
+	c:= client.New(handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &r})))
+
+	var resp struct {
+		CreateRecord struct {
+			Student string
+			Course string
+			Startdate string
+			Finishdate string
+		}
+	}
+
+	c.MustPost(`
+	mutation {
+		createRecord(student: "12453124", course: "PHP", startdate: "2022-09-01", finishdate: "2022-12-01"){
+			student
+			course
+			startdate
+			finishdate
+		}
+	}`, &resp)
+	
+
+
+	require.Equal(t, "12453124", resp.CreateRecord.Student)
+	require.Equal(t, "PHP", resp.CreateRecord.Course)
+	require.Equal(t, "2022-09-01", resp.CreateRecord.Startdate)
+	require.Equal(t, "2022-12-01", resp.CreateRecord.Finishdate)
+
+
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expectations: %s", err)
+	}
+	
 }
 
 func TestUpdateStudent(t *testing.T) {
