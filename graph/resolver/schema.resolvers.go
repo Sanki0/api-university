41baef96cdae9539ec232ca8d6450a4dc9fc23f9
Sanki0 <sebastian.ferreyra.c@uni.pe>
2022-08-08 17:50:10 -0500

    feat: added new file for mutation & query struct

--- graph/resolver/schema.resolvers.go
+++ graph/resolver/schema.resolvers.go
@@ -6,25 +6,8 @@ package resolver
 import (
 	"context"
 
-	"github.com/Sanki0/api-university/graph/generated"
-
 	"github.com/Sanki0/api-university/graph/model"
 )
-
-// CreateStudent is the resolver for the createStudent field.
-func (r *mutationResolver) CreateStudent(ctx context.Context, nombre *string, dni string, direccion *string, fechaNacimiento *string) (*model.Student, error) {
-	student := model.Student{Nombre: nombre, Dni: dni, Direccion: direccion, FechaNacimiento: fechaNacimiento}
-
-	_, err := r.DB.Exec("INSERT INTO students(Nombre, Dni, Direccion, Fecha_nacimiento) VALUES(?, ?, ?, ?)",
-		student.Nombre, student.Dni, student.Direccion, student.FechaNacimiento)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &student, nil
-
-}
 
 // CreateCourse is the resolver for the createCourse field.
 func (r *mutationResolver) CreateCourse(ctx context.Context, nombre string, descripcion *string, temas *string) (*model.Course, error) {
@@ -53,22 +36,6 @@ func (r *mutationResolver) CreateRecord(ctx context.Context, student string, cou
 		return nil, err
 	}
 	return &record, nil
-}
-
-// UpdateStudent is the resolver for the updateStudent field.
-func (r *mutationResolver) UpdateStudent(ctx context.Context, nombre *string, dni string, direccion *string, fechaNacimiento *string) (*model.Student, error) {
-
-	student := model.Student{Dni: dni}
-
-	_, err := r.DB.Exec("UPDATE students SET Nombre=?, Dni=?, Direccion=?, Fecha_nacimiento=? WHERE dni=?",
-		nombre, dni, direccion, fechaNacimiento, dni)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &student, nil
-
 }
 
 // UpdateCourse is the resolver for the updateCourse field.
@@ -103,20 +70,6 @@ func (r *mutationResolver) UpdateRecord(ctx context.Context, student string, cou
 	return &record, nil
 }
 
-// DeleteStudent is the resolver for the deleteStudent field.
-func (r *mutationResolver) DeleteStudent(ctx context.Context, dni string) (*model.Student, error) {
-
-	student := model.Student{Dni: dni}
-
-	_, err := r.DB.Exec("DELETE FROM students WHERE dni = ?", dni)
-
-	if err != nil {
-		return nil, err
-	}
-
-	return &student, nil
-}
-
 // DeleteCourse is the resolver for the deleteCourse field.
 func (r *mutationResolver) DeleteCourse(ctx context.Context, nombre string) (*model.Course, error) {
 
@@ -143,29 +96,6 @@ func (r *mutationResolver) DeleteRecord(ctx context.Context, student string, cou
 	}
 
 	return &record, nil
-}
-
-// GetStudents is the resolver for the getStudents field.
-func (r *queryResolver) GetStudents(ctx context.Context) ([]*model.Student, error) {
-
-	var students []*model.Student
-	rows, err := r.DB.Query("SELECT * FROM students")
-
-	if err != nil {
-		return nil, err
-	}
-
-	defer rows.Close()
-
-	for rows.Next() {
-		var s model.Student
-		if err := rows.Scan(&s.Nombre, &s.Dni, &s.Direccion, &s.FechaNacimiento); err != nil {
-			return nil, err
-		}
-		students = append(students, &s)
-	}
-
-	return students, nil
 }
 
 // GetCourses is the resolver for the getCourses field.
@@ -214,20 +144,6 @@ func (r *queryResolver) GetRecords(ctx context.Context) ([]*model.Record, error)
 	return records, nil
 }
 
-// GetStudent is the resolver for the getStudent field.
-func (r *queryResolver) GetStudent(ctx context.Context, dni string) (*model.Student, error) {
-
-	student := model.Student{Dni: dni}
-
-	err := r.DB.QueryRow("SELECT * FROM students WHERE dni = ?",
-		dni).Scan(&student.Nombre, &student.Dni, &student.Direccion, &student.FechaNacimiento)
-	if err != nil {
-		return nil, err
-	}
-
-	return &student, nil
-}
-
 // GetCourse is the resolver for the getCourse field.
 func (r *queryResolver) GetCourse(ctx context.Context, nombre string) (*model.Course, error) {
 
@@ -257,12 +173,3 @@ func (r *queryResolver) GetRecord(ctx context.Context, student string, course st
 
 	return &record, nil
 }
-
-// Mutation returns generated.MutationResolver implementation.
-func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
-
-// Query returns generated.QueryResolver implementation.
-func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
-
-type mutationResolver struct{ *Resolver }
-type queryResolver struct{ *Resolver }
---
+++ graph/resolver/schema.resolvers.student.go
@@ -0,0 +1,92 @@
+package resolver
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.
+
+import (
+	"context"
+
+	"github.com/Sanki0/api-university/graph/model"
+)
+
+// CreateStudent is the resolver for the createStudent field.
+func (r *mutationResolver) CreateStudent(ctx context.Context, nombre *string, dni string, direccion *string, fechaNacimiento *string) (*model.Student, error) {
+	student := model.Student{Nombre: nombre, Dni: dni, Direccion: direccion, FechaNacimiento: fechaNacimiento}
+
+	_, err := r.DB.Exec("INSERT INTO students(Nombre, Dni, Direccion, Fecha_nacimiento) VALUES(?, ?, ?, ?)",
+		student.Nombre, student.Dni, student.Direccion, student.FechaNacimiento)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &student, nil
+
+}
+
+// UpdateStudent is the resolver for the updateStudent field.
+func (r *mutationResolver) UpdateStudent(ctx context.Context, nombre *string, dni string, direccion *string, fechaNacimiento *string) (*model.Student, error) {
+
+	student := model.Student{Dni: dni}
+
+	_, err := r.DB.Exec("UPDATE students SET Nombre=?, Dni=?, Direccion=?, Fecha_nacimiento=? WHERE dni=?",
+		nombre, dni, direccion, fechaNacimiento, dni)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &student, nil
+
+}
+
+// DeleteStudent is the resolver for the deleteStudent field.
+func (r *mutationResolver) DeleteStudent(ctx context.Context, dni string) (*model.Student, error) {
+
+	student := model.Student{Dni: dni}
+
+	_, err := r.DB.Exec("DELETE FROM students WHERE dni = ?", dni)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &student, nil
+}
+
+// GetStudents is the resolver for the getStudents field.
+func (r *queryResolver) GetStudents(ctx context.Context) ([]*model.Student, error) {
+
+	var students []*model.Student
+	rows, err := r.DB.Query("SELECT * FROM students")
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var s model.Student
+		if err := rows.Scan(&s.Nombre, &s.Dni, &s.Direccion, &s.FechaNacimiento); err != nil {
+			return nil, err
+		}
+		students = append(students, &s)
+	}
+
+	return students, nil
+}
+
+// GetStudent is the resolver for the getStudent field.
+func (r *queryResolver) GetStudent(ctx context.Context, dni string) (*model.Student, error) {
+
+	student := model.Student{Dni: dni}
+
+	err := r.DB.QueryRow("SELECT * FROM students WHERE dni = ?",
+		dni).Scan(&student.Nombre, &student.Dni, &student.Direccion, &student.FechaNacimiento)
+	if err != nil {
+		return nil, err
+	}
+
+	return &student, nil
+}
